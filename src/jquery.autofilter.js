aa844fa810054fe7c15a0e4174b8a53615fd6484
Josh Stratton <strattonbrazil@gmail.com>
2017-09-12 23:50:42 -0700

    refactored to handle table updates

--- src/jquery.autofilter.js
+++ src/jquery.autofilter.js
@@ -21,7 +21,7 @@
     // has a cell that isn't approved by the filter for its
     // respective column
     function updateRowVisibility(table, filters) {
-        let tableBody = $(table).find("tbody");
+        const tableBody = $(table).find("tbody");
         tableBody.find("tr").each(function(rowIndex) {
             let rowVisible = true;
             $(this).find("td").each(function(cellColumnIndex) {
@@ -34,6 +34,33 @@
                 $(this).hide();
             }
         });
+    }
+
+    // given an HTML table object, attach a "column filters" view model to it, which
+    // is an array of arrays--each subelement being a possible filter
+    function resetColumnFilters(table) {
+        const numColumns = $(table).find("thead").find("td").length;
+        const rows = getTableData(table);
+        const allColumnFilters = [];
+        for (let columnIndex = 0; columnIndex < numColumns; columnIndex++) {
+            const filterable = {};
+            const filterableList = [];
+
+            for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+                const row = rows[rowIndex];
+
+                let data = rows[rowIndex][columnIndex];
+                if (!(data in filterable)) {
+                    filterable[data] = true;
+                    filterableList.push({
+                        "label" : data,
+                        "checked" : true
+                    });
+                }
+            }
+            allColumnFilters.push(filterableList);
+        }
+        table._allColumnFilters = allColumnFilters;
     }
 
     $.fn.autofilter = function() {
@@ -60,78 +87,69 @@
             const table = this;
             let tableBody = $(this).find("tbody");
 
-            // get table data
-            let rows = getTableData(this);
-
             // add filter element to all top tds
             let headerRow = $(this).find("thead");
             
             // attach column filters to table
             var allColumnFilters = [];
 
+            resetColumnFilters(table);
+
+            // add filter icons to each header
             var headers = headerRow.find("td");
             headers.each(function(columnIndex) {
-                var header = $(this);
-                header.append("<div class='filter-icon-wrapper'><div class='filter-icon'></div></div>");
-                var filterIcon = header.find(".filter-icon");
+                const header = $(this);
+                if (header.find(".filter-icon-wrapper").length > 0) { // autofilter() called again, reset with new data
+                    tableBody.find("tr").show(); // show all rows
+                } else { // no icon yet, create it
+                    header.append("<div class='filter-icon-wrapper'><div class='filter-icon'></div></div>");
+                    var filterIcon = header.find(".filter-icon");
 
-                // add to the create the data model
-                let filterable = {};
-                let filterableList = [];
-                for (var rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-                    let data = rows[rowIndex][columnIndex];
-                    if (!(data in filterable)) {
-                        filterable[data] = true;
-                        filterableList.push({
-                            "label" : data,
-                            "checked" : true
-                        });
-                    }
-                }
-                allColumnFilters.push(filterableList);
+                    filterIcon.click(function() {
+                        const allColumnFilters = table._allColumnFilters;
 
-                filterIcon.click(function() {
-                    // empty the list and put in the filters for this column
-                    $("#filter-list").html("");
-                    var columnFilters = allColumnFilters[columnIndex];
-                    for (var filterIndex = 0; filterIndex < columnFilters.length; filterIndex++) {
-                        let filter = columnFilters[filterIndex];
-                        let checkId = "autofilter_check" + filterIndex;
-                        let checkedStr = filter["checked"] ? " checked" : "";
-                        $("#filter-list").append("<input id='" + checkId + "' type='checkbox'" + checkedStr + "><label>" + filter["label"] + "</label><br/>");
-                    }
-                    
-                    modalWidget.modal();
-                    modalWidget.one($.modal.BEFORE_CLOSE, function(event, modal) {
-                        // save off check changes
-                        var includeAll = true;
+                        // empty the list and put in the filters for this column
+                        $("#filter-list").html("");
+                        var columnFilters = allColumnFilters[columnIndex];
                         for (var filterIndex = 0; filterIndex < columnFilters.length; filterIndex++) {
                             let filter = columnFilters[filterIndex];
                             let checkId = "autofilter_check" + filterIndex;
-                            let isIncluding = $("#" + checkId).is(':checked');
-                            columnFilters[filterIndex]["checked"] = isIncluding;
-                            includeAll = includeAll && isIncluding;
+                            let checkedStr = filter["checked"] ? " checked" : "";
+                            $("#filter-list").append("<input id='" + checkId + "' type='checkbox'" + checkedStr + "><label>" + filter["label"] + "</label><br/>");
                         }
+                        
+                        modalWidget.modal();
+                        modalWidget.one($.modal.BEFORE_CLOSE, function(event, modal) {
+                            // save off check changes
+                            var includeAll = true;
+                            for (var filterIndex = 0; filterIndex < columnFilters.length; filterIndex++) {
+                                let filter = columnFilters[filterIndex];
+                                let checkId = "autofilter_check" + filterIndex;
+                                let isIncluding = $("#" + checkId).is(':checked');
+                                columnFilters[filterIndex]["checked"] = isIncluding;
+                                includeAll = includeAll && isIncluding;
+                            }
 
-                        // update filter icon's status
-                        if (includeAll) {
-                            filterIcon.removeClass("filtering");
-                        } else {
-                            filterIcon.addClass("filtering");
-                        }
+                            // update filter icon's status
+                            if (includeAll) {
+                                filterIcon.removeClass("filtering");
+                            } else {
+                                filterIcon.addClass("filtering");
+                            }
 
-                        // build the check set
-                        var allColumnFilterSets = allColumnFilters.map(function(columnFilters) {
-                            return new Set(columnFilters.filter(function(filter) {
-                                return filter["checked"];
-                            }).map(function(filter){ 
-                                return filter["label"];
-                            }));
+                            // build the filter set for each column
+                            var allColumnFilterSets = allColumnFilters.map(function(columnFilters) {
+                                return new Set(columnFilters.filter(function(filter) {
+                                    return filter["checked"];
+                                }).map(function(filter){ 
+                                    return filter["label"];
+                                }));
+                            });
+
+                            updateRowVisibility(table, allColumnFilterSets);
                         });
-
-                        updateRowVisibility(table, allColumnFilterSets);
                     });
-                });
+                }
             });
         });
     }
